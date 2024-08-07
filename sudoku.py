5e2c88b88e6d8127419964b230d1757b091f02d1
Justin Daneau <jdaneau87@gmail.com>
2024-08-07 14:10:51 +0200

    uh oh... I sense an impending conflict

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,6 @@
-print("Hello World!")
+print("Hello World...") # changed to be less enthusiastic
 for i in range(10):
 	print("This is the "+str(i)+"th message!")
 
 print("You found the end! Bye bye!")
+exit() # very necessary, earth blows up if this isnt included
